2fa92a4910cbfbbe54018528f2b5cbec314ebd7f
marmaryia <marozavamaryia@gmail.com>
2025-02-28 19:11:00 +0000

    change convertTimestampToDate to default to current time if no timestamp available

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,8 +1,8 @@
 const db = require("../../db/connection");
 
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
-  if (!created_at) return { ...otherProperties };
-  return { created_at: new Date(created_at), ...otherProperties };
+  const date = created_at ? new Date(created_at) : new Date();
+  return { created_at: date, ...otherProperties };
 };
 
 exports.mapDataForInsertion = (array, ...keys) => {
