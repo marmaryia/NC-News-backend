f1dd8b96c83eeb54ed356d01cd55a695dd0189d2
marmaryia <marozavamaryia@gmail.com>
2025-03-10 11:09:04 +0000

    add response with 200 for endpoint /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,6 +39,34 @@ describe("GET /api/topics", () => {
   });
 });
 
+describe("GET /api/articles/:article_id", () => {
+  test("200: Responds with the article at the requested article ID", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then(({ body: { article } }) => {
+        const {
+          article_id,
+          title,
+          topic,
+          author,
+          body,
+          created_at,
+          votes,
+          article_img_url,
+        } = article;
+        expect(article_id).toBe(3);
+        expect(typeof title).toBe("string");
+        expect(typeof topic).toBe("string");
+        expect(typeof author).toBe("string");
+        expect(typeof body).toBe("string");
+        expect(typeof created_at).toBe("string");
+        expect(typeof votes).toBe("number");
+        expect(typeof article_img_url).toBe("string");
+      });
+  });
+});
+
 describe("GET /aqi", () => {
   test("404: Responds with a message about the endpoint not existing", () => {
     return request(app)
--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const {
   handleServerErrors,
 } = require("./controllers/errors.controllers");
 const { getAllTopics } = require("./controllers/topics.controllers");
+const { getArticleById } = require("./controllers/articles.controllers");
 const app = express();
 
 app.get("/api", getAvailableEndpoints);
 
 app.get("/api/topics", getAllTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 app.all("/*", handleNonexistentPath);
 
