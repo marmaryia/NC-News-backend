1a02ccc2689f2a5e83792138afec35b248ea8127
marmaryia <marozavamaryia@gmail.com>
2025-03-10 10:39:35 +0000

    add server error handler

--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,3 +1,7 @@
 exports.handleNonexistentPath = (request, response) => {
   response.status(404).send({ msg: "This endpoint does not exist." });
 };
+
+exports.handleServerErrors = (err, request, response, next) => {
+  response.status(500).send({ msg: "Something went wrong :(" });
+};
