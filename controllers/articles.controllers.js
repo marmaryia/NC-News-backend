22da8669726e37cf7cc3c7a969769d17ad5fae97
marmaryia <marozavamaryia@gmail.com>
2025-03-10 11:24:56 +0000

    remove console.log

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -4,7 +4,6 @@ exports.getArticleById = (request, response, next) => {
   const { article_id } = request.params;
   fetchArticleById(article_id)
     .then((article) => {
-      console.log(article);
       response.status(200).send({ article });
     })
     .catch((err) => {
