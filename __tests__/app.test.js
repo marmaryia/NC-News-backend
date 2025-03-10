6a2a4ce99705908d930fd2737098e0d272a5ec97
marmaryia <marozavamaryia@gmail.com>
2025-03-10 10:14:17 +0000

    add tests for /api and incorrect path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,27 @@
+const app = require("../app");
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const request = require("supertest");
 
 /* Set up your beforeEach & afterAll functions here */
 
 describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("GET /aqi", () => {
+  test("404: Responds with a message about the endpoint not existing", () => {
+    return request(app)
+      .get("/aqi")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("This endpoint does not exist.");
       });
   });
 });
