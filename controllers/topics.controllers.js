45d8c03f6f2e32fa7b41e0da1a63f8077dde6f64
marmaryia <marozavamaryia@gmail.com>
2025-03-12 16:32:09 +0000

    add handling of post requests to /api/topics

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,7 +1,19 @@
-const { fetchAllTopics } = require("../models/topics.models");
+const { fetchAllTopics, addTopic } = require("../models/topics.models");
 
 exports.getAllTopics = (request, response) => {
   fetchAllTopics().then((topics) => {
     response.status(200).send({ topics });
   });
+};
+
+exports.postTopic = (request, response, next) => {
+  const { slug, description } = request.body;
+  addTopic(slug, description)
+    .then((topic) => {
+      response.status(201).send({ topic });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
