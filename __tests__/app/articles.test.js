07fb0acd3ae8194d25aaf79ab5872700e2512dc0
marmaryia <marozavamaryia@gmail.com>
2025-03-13 10:45:20 +0000

    add error handling for when data violates database constrainsts

--- __tests__/app/articles.test.js
+++ __tests__/app/articles.test.js
@@ -246,6 +246,22 @@ describe("POST /api/articles", () => {
         expect(msg).toBe("Nothing found with this identifier.");
       });
   });
+  test("400: Responds with 'Bad Request' if the data provided violates database constraints", () => {
+    return request(app)
+      .post("/api/articles")
+      .send({
+        author: "lurker",
+        title: "New Article",
+        body: "New article text",
+        topic: "cats",
+        article_img_url:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam qu",
+      })
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad request: invalid input");
+      });
+  });
 });
 
 describe("GET /api/articles/:article_id", () => {
